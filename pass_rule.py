31cf8c952d7d21598467233f7cc12bf1df8f1c30
robert <root@cnmoker.org>
2015-06-09 16:57:50 +0800

    bugfix

--- pass_rule.py
+++ pass_rule.py
@@ -1,13 +1,25 @@
+#!/usr/bin/env python
+#coding=utf-8
+#by robert
+"""
+1、密码长度在8-12位之间
+2、密码全是数字
+3、密码全是小写字母
+4、密码全是大写字母
+5、密码中存在连续相同的数字或字母，并且数量大于3，例如1111、aaaa
+6、密码中存在连续数字，并且长度超过3个，例如1234、4567
+
+详情见:http://drops.cnmoker.org/archives/361.html
+"""
 import sys
 import re
-import str
 
 def is_legal_pwd(pwd):
     for idx, s in enumerate(pwd):
         if s.isdigit() and pwd[idx:idx+4].isdigit() and int(pwd[idx:idx+4]) - int(s*4) == 123: return False
 
 def pass_rule():
-    newfile=open("c:\\newpass.txt", 'wb')
+    newfile=open("c:\\newpass.txt", 'w')
     for line in open("c:\\csdn_pwd_top30000.txt"):
         line=str(line).strip()
         if 13>len(line)>7:
